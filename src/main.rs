0a3194b1d462edb514043babc2b835b109c87da1
Jakub Rękas <jakub.rekas@swmansion.com>
2025-11-16 17:25:43 +0100

    Multiblock example

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,11 @@ fn main() {
     let hash = Sha1Digest::hash(msg_bytes);
     println!("{:?}", hash.bytes());
     println!("{}", hash.hex_digest());
+
+    let msg = "a".repeat(70);
+    println!("{msg}");
+    let msg_bytes = msg.as_bytes().to_vec();
+    let hash = Sha1Digest::hash(msg_bytes);
+    println!("{:?}", hash.bytes());
+    println!("{}", hash.hex_digest());
 }
